aab4d7157ba8bac63bb77711141a8c87cdd4a103
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-02-10 20:40:30 -0500

    minor

--- milvus_runner.py
+++ milvus_runner.py
@@ -19,11 +19,11 @@ def main():
     assert os.path.exists(docker_compose_file_path)
 
     if args.command == "start":
-        command = f"echo <password> | sudo -S docker-compose up -d"
+        command = f"echo <password> | sudo -S docker-compose -f {docker_compose_file_path} up -d"
     elif args.command == "stop":
-        command = f"echo <password> | sudo -S docker-compose down"
+        command = f"echo <password> | sudo -S docker-compose -f {docker_compose_file_path} down"
     elif args.command == "status":
-        command = f"echo <password> | sudo -S docker-compose ps"
+        command = f"echo <password> | sudo -S docker-compose -f {docker_compose_file_path} ps"
     elif args.command == "delete":
         # all the data is stored here (similar to ES, so don't delete unless really required)
         volumes_directory = os.path.join(os.environ["MILVUS_DATA_DIRECTORY"], "volumes")
