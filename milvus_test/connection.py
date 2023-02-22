cb00773d1b706d03dc36c7f70258c18045904398
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-02-22 17:38:49 -0500

    minor

--- milvus_test/connection.py
+++ milvus_test/connection.py
@@ -7,8 +7,8 @@ def test_postgresql_connection():
     from sqlalchemy import create_engine
 
     # Note the address shouldn't have http://
-    # Note the default psql port is 5432 but I changed it to 8432 to avoid conflict on beaker.
     postgresql_host, postgresql_port = os.environ.get("POSTGRESQL_SERVER_ADDRESS", "localhost:5432").split(":")
+    assert "//" not in postgresql_host
 
     try:
         engine = create_engine(f"postgresql://postgres:postgres@{postgresql_host}:{postgresql_port}/postgres")
@@ -24,6 +24,7 @@ def test_milvus_connection():
 
     # Note the address shouldn't have http://
     milvus_host, milvus_port = os.environ.get("MILVUS_SERVER_ADDRESS", "localhost:19530").split(":")
+    assert "//" not in milvus_host
 
     try:
         connections.add_connection(default={"host": milvus_host, "port": milvus_port})
