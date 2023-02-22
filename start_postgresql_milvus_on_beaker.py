ce58820300ee33a00c4585c6f8cb17da0ec92651
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-02-22 17:47:44 -0500

    minor

--- start_postgresql_milvus_on_beaker.py
+++ start_postgresql_milvus_on_beaker.py
@@ -48,23 +48,46 @@ def make_image(update_if_exists: bool = False):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Start Postgresql and Milvus servers on Beaker interactive session.")
+    parser = argparse.ArgumentParser(
+        description="Do start/stop/status on Postgresql and Milvus servers on Beaker interactive session."
+    )
     parser.add_argument("--force", action="store_true", help="delete the image if it already exists.")
+    parser.add_argument(
+        "command", type=str, help="command", choices=("start", "stop", "status")
+    )
     args = parser.parse_args()
 
-    make_image(args.force)
-    print("Done with potential image creation.")
+    if args.command == "start":
 
-    user_name = get_beaker_config()["user_name"]
-    beaker_workspace = get_beaker_config()["beaker_workspace"]
+        make_image(args.force)
+        print("Done with potential image creation.")
 
-    command = f'''
+        user_name = get_beaker_config()["user_name"]
+        beaker_workspace = get_beaker_config()["beaker_workspace"]
+
+        command = f'''
 beaker session create \
     --image beaker://{user_name}/{image_name} \
     --workspace {beaker_workspace}
-    '''.strip()
-    print(f"Running: {command}")
-    subprocess.run(command, shell=True)
+        '''.strip()
+        print(f"Running: {command}")
+        subprocess.run(command, shell=True)
+
+    if args.command == "stop":
+        assert args.force, "--force is meaningless for stop command."
+        # This is usually not needed. Run it only if you get an error message saying
+        # the ports are in use or something.
+        command = (
+            "docker rm -f postgres -f milvus-standalone -f milvus-minio -f milvus-etcd"
+        )
+        print(command)
+        subprocess.run(command, shell=True)
+
+    if args.command == "status":
+        assert args.force, "--force is meaningless for status command."
+        command = "docker ps -a"
+        print(command)
+        subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
