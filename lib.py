af2da68b8968fa3e0fd92a76b178eb246e821fd8
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-03-21 22:41:11 -0400

    minor

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import os
 import json
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Any
 
 
 def read_json(file_path: str) -> Union[List, Dict]:
@@ -25,3 +25,34 @@ def is_directory_empty(directory: str) -> bool:
     if not os.path.exists(directory):
         return True
     return not os.listdir(directory)
+
+def flatten_dict(input_dict: Dict) -> Dict[str, Any]:
+    """Returns allennlp-params-styled flat dict."""
+    flat_dict = {}
+    def recurse(dict_, path):
+        for key, value in dict_.items():
+            newpath = path + [key]
+            if isinstance(value, dict):
+                recurse(value, newpath)
+            else:
+                flat_dict[".".join(newpath)] = value
+    recurse(input_dict, [])
+    return flat_dict
+
+
+def get_wandb_configs() -> Dict:
+    file_path = ".project-wandb-config.json"
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path) as file:
+        wand_configs = json.load(file)
+    return wand_configs
+
+
+def get_beaker_configs() -> Dict:
+    file_path = ".project-beaker-config.json"
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path) as file:
+        beaker_configs = json.load(file)
+    return beaker_configs
