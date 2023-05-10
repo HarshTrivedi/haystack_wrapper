7056cfa870cfb19ee9e5feb25c04c59f876e5bb5
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-05-10 10:15:30 -0400

    minor

--- occupy_cpu.py
+++ occupy_cpu.py
@@ -1,14 +1,15 @@
-# import time
+import time
 
 
 def main():
     print("Starting a process to keep one CPU occupied.")
     while True:
-        x = 1
-        y = 2
-        z = x + y
-        # time.sleep(0.2)
-
+        for _ in range(100000000):
+            x = 1
+            y = 2
+            z = x + y
+        time.sleep(0.01)
+        print("Running again.")
 
 if __name__ == '__main__':
     main()
