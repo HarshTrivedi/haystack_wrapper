fbaf2bb00c51c20deab59d6925bc5e6c26468db0
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-05-10 12:07:42 -0400

    minor

--- haystack_monkeypatch.py
+++ haystack_monkeypatch.py
@@ -1,12 +1,16 @@
 # Monkey Patch to fix the progressbar in haystack prediction for farm-haystack[milvus]==v1.15.0
 
-from typing import Optional, Dict, List, Union
+from tqdm import tqdm
+from typing import Optional, Dict, List, Union, Any
 
 import numpy as np
+import torch
+from torch.utils.data.sampler import SequentialSampler
 
 from haystack.schema import Document, FilterType
 from haystack.errors import HaystackError
 from haystack.document_stores import BaseDocumentStore
+from haystack.modeling.data_handler.dataloader import NamedDataLoader
 
 
 def query_by_embedding_batch(
@@ -90,3 +94,61 @@ def retrieve_batch(
 
 from haystack.nodes import DensePassageRetriever
 DensePassageRetriever.retrieve_batch = retrieve_batch
+
+
+def _get_predictions(self, dicts: List[Dict[str, Any]]) -> Dict[str, np.ndarray]:
+
+    dataset, tensor_names, _, _ = self.processor.dataset_from_dicts(
+        dicts, indices=[i for i in range(len(dicts))], return_baskets=True
+    )
+
+    data_loader = NamedDataLoader(
+        dataset=dataset, sampler=SequentialSampler(dataset), batch_size=self.batch_size, tensor_names=tensor_names
+    )
+    query_embeddings_batched = []
+    passage_embeddings_batched = []
+    self.model.eval()
+
+    # When running evaluations etc., we don't want a progress bar for every single query
+    if len(dataset) == 1:
+        disable_tqdm = True
+    else:
+        disable_tqdm = not self.progress_bar
+
+    # NOTE(Harsh): The next 1 line is the reason for monkey patch.
+    disable_tqdm = True
+    with tqdm(
+        total=len(data_loader) * self.batch_size,
+        unit=" Docs",
+        desc="Create embeddings",
+        position=1,
+        leave=False,
+        disable=disable_tqdm,
+    ) as progress_bar:
+        for raw_batch in data_loader:
+            batch = {key: raw_batch[key].to(self.devices[0]) for key in raw_batch}
+
+            # get logits
+            with torch.inference_mode():
+                query_embeddings, passage_embeddings = self.model.forward(
+                    query_input_ids=batch.get("query_input_ids", None),
+                    query_segment_ids=batch.get("query_segment_ids", None),
+                    query_attention_mask=batch.get("query_attention_mask", None),
+                    passage_input_ids=batch.get("passage_input_ids", None),
+                    passage_segment_ids=batch.get("passage_segment_ids", None),
+                    passage_attention_mask=batch.get("passage_attention_mask", None),
+                )[0]
+                if query_embeddings is not None:
+                    query_embeddings_batched.append(query_embeddings.cpu().numpy())
+                if passage_embeddings is not None:
+                    passage_embeddings_batched.append(passage_embeddings.cpu().numpy())
+            progress_bar.update(self.batch_size)
+
+    all_embeddings: Dict[str, np.ndarray] = {}
+    if passage_embeddings_batched:
+        all_embeddings["passages"] = np.concatenate(passage_embeddings_batched)
+    if query_embeddings_batched:
+        all_embeddings["query"] = np.concatenate(query_embeddings_batched)
+    return all_embeddings
+
+DensePassageRetriever._get_predictions = _get_predictions
