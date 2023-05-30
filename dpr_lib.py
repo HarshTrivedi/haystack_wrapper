aea9fb2334c3ea7b719021570e0a381907702d34
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-05-29 20:02:20 -0400

    make indexing configurable

--- dpr_lib.py
+++ dpr_lib.py
@@ -40,15 +40,35 @@ def build_document_store(
     milvus_port: str,
     index_name: str,
     index_type: str
-    # TODO: Pass other parameters also.
 ):
     from haystack.document_stores import MilvusDocumentStore
+    # See parameters required for index_type in ...
+    # There are two kinds of parameters: ones required at construction/indexing time, and ones required at search time.
+
+    # IVF_FLAT => nlist | nprobe
+    # HNSW => M, efConstruction | ef
+
+    # Info related to IVF_FLAT:
     # nlist is set at the indexing time and nprobe is set at the search time (https://milvus.io/docs/index.md).
     # A bug in milvus store makes passing index_param necessary (https://github.com/deepset-ai/haystack/issues/4681)
     # Check this out to see how to set the nlist and nprobe parameters:
     # https://milvus.io/docs/v1.1.1/performance_faq.md#How-to-set-nlist-and-nprobe-for-IVF-indexes
     # The nlist=16348 and nprob=512 values are set based on the curve given there. May be revisit it at some point.
     # On increasing index_file_size, indexing will slow, but querying will be fast. https://milvus.io/docs/v1.1.1/tuning.md
+
+    # Info related to HNSW:
+    # DPR paper has used this: 512, 200 | 128 (see footnote 10)
+    # Haystack benchmark uses (https://haystack.deepset.ai/benchmarks/v0.9.0): 128, 80 | 20
+    # milvus caps M to be maximum 64, so I can't increase it beyond that.
+    if index_type == "IVF_FLAT"
+        index_param = {"nlist": 16384}
+        search_param = {"nprobe": 512}
+    elif index_type == "HNSW":
+        index_param = {"M": 512, "efConstruction": 200}
+        search_param = {"ef": 128}
+    else:
+        raise Exception(f"index_type must be IVF_FLAT or HNSW, found {index_type}")
+
     document_store = MilvusDocumentStore(
         sql_url=f"postgresql://postgres:postgres@{postgresql_host}:{postgresql_port}/postgres",
         host=milvus_host, port=milvus_port,
@@ -56,8 +76,8 @@ def build_document_store(
         embedding_dim=768, id_field="id", embedding_field="embedding",
         progress_bar=True,
         index_file_size=1024,
-        index_param={"nlist": 16384},
-        search_param={"params": {"nprobe": 512}},
+        index_param=index_param,
+        search_param={"params": search_param}, # yeah, there's a bug due to which "params" is also needed!
         similarity="dot_product",
     )
     return document_store
