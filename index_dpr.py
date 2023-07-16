23c7a43aa55633ec2358164484eb5d2523c265f9
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-07-16 10:08:33 -0400

    debug

--- index_dpr.py
+++ index_dpr.py
@@ -101,6 +101,8 @@ def main():
         )
     monkeypatch_retriever(retriever)
 
+    exit("Exiting successfully.")
+
     for slice_index in range(index_num_chunks):
 
         print(f"\n\nReading input documents slice {slice_index+1}/{index_num_chunks}.")
