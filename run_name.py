0eb787bda0d31da3025f9a843bcb53960657d3e4
Harsh Trivedi <harshjtrivedi94@gmail.com>
2023-08-10 22:25:21 -0400

    minor

--- run_name.py
+++ run_name.py
@@ -11,11 +11,13 @@ def get_run_name(command: str, experiment_name: str, dataset_filepath: str = "")
     text2hash = lambda text: str(
         int(hashlib.sha256(text.encode("utf-8")).hexdigest(), 16) % 10**8
     )
-    full_identifier = text2hash(experiment_name + dataset_filepath)
-    title = experiment_name.split("__")[0]
+    full_identifier = text2hash(command + experiment_name + dataset_filepath)
+    if dataset_filepath:
+        title = f"{command}__{experiment_name.split('__')[0]}__{dataset_filepath.replace('/', '__')}"
+    else:
+        title = f"{command}__{experiment_name.split('__')[0]}"
+    beaker_run_name = title
     if len(title) > 100:
         print("Warning: Experiment name can't more than 115 characters, so shortening it.")
-        experiment_name = f"{command}__{title[:100]}__{full_identifier}"
-    else:
-        experiment_name = f"{command}__{title}"
-    return experiment_name
+        beaker_run_name = f"{title[:95]}__{full_identifier}"
+    return beaker_run_name
